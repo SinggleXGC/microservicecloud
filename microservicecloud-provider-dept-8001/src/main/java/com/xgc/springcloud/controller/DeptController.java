438fbbacd5864b7d42af2a7aad085c1fda5cd190
SinggleXGC <1307708161@qq.com>
2020-08-18 21:41:22 +0800

    部门提供者：服务发现配置

--- microservicecloud-provider-dept-8001/src/main/java/com/xgc/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/xgc/springcloud/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.xgc.springcloud.controller;
 import com.xgc.springcloud.entity.Dept;
 import com.xgc.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,6 +14,8 @@ public class DeptController {
 
     @Autowired
     private DeptService deptService;
+    @Autowired
+    private DiscoveryClient client;
 
     @RequestMapping(value = "/dept/add", method = RequestMethod.POST)
     public boolean add(@RequestBody Dept dept) {
@@ -26,5 +30,19 @@ public class DeptController {
     @RequestMapping(value = "/dept/list", method = RequestMethod.GET)
     public List<Dept> list() {
         return deptService.list();
+    }
+
+    @RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+    public Object discovery() {
+        //获取Eureka中注册了的微服务
+        List<String> list = client.getServices();
+        System.out.println("***********" + list);
+
+        //获取serviceId 为 MICROSERVICECLOUD-DEPT的服务
+        List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD-DEPT");
+        for (ServiceInstance element : srvList) {
+            System.out.println(element.getInstanceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t" + element.getUri());
+        }
+        return this.client;
     }
 }
